416bd85003c422bed0235400af8912c01f0ee74e
Harsh-BH <harsh.aby.007@gmail.com>
2025-02-07 13:50:18 +0530

    first commit

--- src/app/components/ConvexClientProvider.tsx
+++ src/app/components/ConvexClientProvider.tsx
@@ -3,7 +3,12 @@ import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
+
+if (!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY is not defined");
+}
+
 
 export default function ConvexClientProvider({
     children,
@@ -11,7 +16,8 @@ export default function ConvexClientProvider({
     children: React.ReactNode;
   }>) {
     return (
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+
             <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
                 {children}
             </ConvexProviderWithClerk>
